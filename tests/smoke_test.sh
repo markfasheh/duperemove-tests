0e858bd64252b82319d7796744206d9d37f9915b
Mark Fasheh <mfasheh@suse.de>
2016-08-05 15:48:55 -0700

    Add some basic tests we can run before a commit
    Signed-off-by: Mark Fasheh <mfasheh@suse.de>

---
+++ tests/smoke_test.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+DEST=/btrfs/basic
+HASHFILE=$DEST/test.hash
+DUPEREMOVE=/build/mfasheh/duperemove.git/duperemove
+MKZEROS=/build/mfasheh/duperemove-tests.git/src/mkzeros
+BS=$((128*1024))
+FDUPES=fdupes
+SIZE=$((4*1024*1024))
+
+prep_dirs() {
+    rm -fr $DEST
+    mkdir -p $DEST
+
+    for i in `seq -w 0 2`;
+    do
+	DIR="$DEST/testdir_$i"
+	mkdir -p $DIR/
+	$MKZEROS -s $SIZE $DIR/0 $DIR/1 $DIR/2 $DIR/3
+    done
+
+    wait
+
+    btrfs fi sync $DEST
+}
+
+prep_secondary_dirs() {
+    #remove a dir to trigger the delete hashes code
+    rm -fr $DEST/testdir_0 &
+    #make a fresh directory so we can add it to the hashfile
+    DIR="$DEST/testdir_extra"
+    mkdir -p $DIR/
+    $MKZEROS -s $SIZE $DIR/0 $DIR/1 $DIR/2 $DIR/3
+    wait
+    btrfs fi sync $DEST
+}
+
+prep_dirs
+
+echo "Test memory only operation"
+#memory only, don't dedupe yet
+$DUPEREMOVE -rhv $DEST/testdir*
+
+echo "Test fdupes mode"
+DIR=$DEST/fdupes
+mkdir -p $DIR
+dd if=/dev/zero of=$DIR/dupefileA.1 bs=$BS count=10
+cp $DIR/dupefileA.1 $DIR/dupefileA.2
+cp $DIR/dupefileA.1 $DIR/dupefileA.3
+dd if=/dev/urandom of=$DIR/dupefileB.1  bs=$BS count=10 iflag=fullblock
+cp $DIR/dupefileB.1 $DIR/dupefileB.2
+$FDUPES -r $DIR | $DUPEREMOVE --fdupes -dv
+
+echo "Test write-hashes"
+$DUPEREMOVE -rhv --write-hashes=$HASHFILE $DEST/testdir*
+
+echo "Test read-hashes"
+$DUPEREMOVE -rhv --read-hashes=$HASHFILE
+
+rm -f $HASHFILE
+
+echo "Test basic hashfile"
+#now store in a hashfile, do dedupe
+$DUPEREMOVE -rdhv --hashfile=$HASHFILE $DEST/testdir*
+
+echo "Test basic hashfile update"
+prep_secondary_dirs
+#dedupe again we should only re-hash testdir_extra
+$DUPEREMOVE -rdhv --hashfile=$HASHFILE $DEST/testdir*
+
+# do it all again in block dedupe mode
+rm -f $HASHFILE
+
+prep_dirs
+prep_secondary_dirs
+
+echo "Test basic hashfile (block dedupe)"
+#now store in a hashfile, do dedupe
+$DUPEREMOVE -rdhv --dedupe-options=block --hashfile=$HASHFILE $DEST/testdir*
