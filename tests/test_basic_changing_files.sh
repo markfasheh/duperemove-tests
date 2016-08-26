ea61de6909f59cfaa6fd7a5015f74caa7de7bf14
Mark Fasheh <mfasheh@suse.de>
2016-08-25 19:40:58 -0700

    Rename issue139.sh, add a couple extra tests to the end
    Signed-off-by: Mark Fasheh <mfasheh@suse.de>

--- tests/test_basic_changing_files.sh
+++ tests/test_basic_changing_files.sh
@@ -11,7 +11,7 @@
 . `dirname $0`/config || exit 1
 . `dirname $0`/common || exit 1
 
-BASEDIR=$TESTDIR/issue139
+BASEDIR=$TESTDIR/changing
 DEST=$BASEDIR/files
 HASHFILE=$BASEDIR/test.hash
 BS=$((128*1024))
@@ -41,7 +41,9 @@ _run_duperemove -rdh --hashfile=$HASHFILE $DEST
 # through as well.
 touch $DEST/testfile_1
 
+# Test for first regression (we should not ENOMEM)
 _run_duperemove -rdh --hashfile=$HASHFILE $DEST
+
 
 # Move a completely new file over an old one
 mv $BASEDIR/testfile_0 $DEST/testfile_2
@@ -51,12 +53,28 @@ _run_duperemove -vrdh --hashfile=$HASHFILE $DEST
 mv $DEST/testfile_3 $DEST/testfile_4
 _run_duperemove -vrdh --hashfile=$HASHFILE $DEST
 
-# Check that the previous run didn't give us duplicate ino/subvol pairs
+# Check that the previous run didn't give us duplicate ino/subvol
+# pairs (2nd regression mentioned at top)
 inos=`echo "select filename, ino, subvol from files group by ino having count(*) > 1;" | sqlite3 $HASHFILE`
 if [ -n "$inos" ]
 then
     echo "TEST FAILURE: duplicated inodes found: $inos"
     exit 1
 fi
+
+#
+# Some very basic tests we don't touch upon above
+#
+
+# remove a file, add a file we already know in place of it
+mv $DEST/testfile_1 $DEST/testfile_5
+# remove a file, add a new file in place of it
+mv $BASEDIR/testfile_1 $DEST/testfile_6
+_run_duperemove -vrdh --hashfile=$HASHFILE $DEST
+
+
+# rename a file to a new location (we get rename over existing file above)
+mv $DEST/testfile_6 $DEST/renamed_file
+_run_duperemove -vrdh --hashfile=$HASHFILE $DEST
 
 exit 0
